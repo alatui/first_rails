a1838af28f9dac23c6b50f41ec6b6fe6e632f512
Fernando Cisneros <fernando.saxofone@gmail.com>
2016-08-18 14:19:56 -0300

    restrictions articles, users, navigation, views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :require_user, except: [:index, :show]
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
 
   def index
     #@articles = Article.all
@@ -54,4 +57,12 @@ class ArticlesController < ApplicationController
     def set_article
       @article = Article.find(params[:id])
     end
+
+    def require_same_user
+      if @article.user != current_user
+        flash[:danger] = 'You can only edit/delete your own article'
+        redirect_to root_path
+      end
+    end
+
 end
